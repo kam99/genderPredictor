bc5d97b4ca98291ee272e8717bbaf9a2704ed198
kam99 <kam99@smtp.ru>
2016-04-21 20:16:18 +0300

    remove "prob" stuff. ~0.854 accuracy features.

--- genderPredictor.py
+++ genderPredictor.py
@@ -9,54 +9,54 @@ import USSSALoader
 import random
 
 class genderPredictor():
-    
+
     def getFeatures(self):
         maleNames,femaleNames=self._loadNames()
-        
+
         featureset = list()
-        
+
         for nameTuple in maleNames:
             features = self._nameFeatures(nameTuple[0])
             featureset.append((features,'M'))
-        
+
         for nameTuple in femaleNames:
             features = self._nameFeatures(nameTuple[0])
             featureset.append((features,'F'))
-    
+
         return featureset
-    
+
     def trainAndTest(self,trainingPercent=0.80):
         featureset = self.getFeatures()
         random.shuffle(featureset)
-        
+
         name_count = len(featureset)
-        
+
         cut_point=int(name_count*trainingPercent)
-        
+
         train_set = featureset[:cut_point]
         test_set  = featureset[cut_point:]
-        
+
         self.train(train_set)
-        
+
         return self.test(test_set)
-        
+
     def classify(self,name):
         feats=self._nameFeatures(name)
         return self.classifier.classify(feats)
-        
+
     def train(self,train_set):
         self.classifier = NaiveBayesClassifier.train(train_set)
         return self.classifier
-        
+
     def test(self,test_set):
        return classify.accuracy(self.classifier,test_set)
-    
+
     def getMostInformativeFeatures(self,n=5):
         return self.classifier.most_informative_features(n)
-        
+
     def _loadNames(self):
         return USSSALoader.getNameList()
-        
+
     def _nameFeatures(self,name):
         name=name.upper()
         return {
@@ -74,4 +74,4 @@ if __name__ == "__main__":
         print '\t%s = %s'%feat
     name = raw_input('Enter name to classify: ')
     print '\n%s is classified as %s'%(name, gp.classify(name))
-     
+
