4ac7552f07156ee70800e2a71bb5fc97eff3b6a9
Alexey Koryakov <akoryakov@academixdirect.com>
2016-04-21 19:38:25 +0300

    remove "prob" stuff. ~0.854 accuracy features.

--- genderPredictor.py
+++ genderPredictor.py
@@ -17,16 +17,10 @@ class genderPredictor():
         
         for nameTuple in maleNames:
             features = self._nameFeatures(nameTuple[0])
-            male_prob, female_prob = self._getProbDistr(nameTuple)
-            features['male_prob'] = male_prob
-            features['female_prob'] = female_prob
             featureset.append((features,'M'))
         
         for nameTuple in femaleNames:
             features = self._nameFeatures(nameTuple[0])
-            male_prob, female_prob = self._getProbDistr(nameTuple)
-            features['male_prob'] = male_prob
-            features['female_prob'] = female_prob
             featureset.append((features,'F'))
     
         return featureset
@@ -57,17 +51,6 @@ class genderPredictor():
     def test(self,test_set):
        return classify.accuracy(self.classifier,test_set)
     
-    def _getProbDistr(self,nameTuple):
-            male_prob = (nameTuple[1] * 1.0) / (nameTuple[1] + nameTuple[2])
-            if male_prob == 1.0:
-                male_prob = 0.99
-            elif male_prob == 0.0:
-                male_prob = 0.01
-            else:
-                pass
-            female_prob = 1.0 - male_prob
-            return (male_prob, female_prob)
-        
     def getMostInformativeFeatures(self,n=5):
         return self.classifier.most_informative_features(n)
         
@@ -77,10 +60,8 @@ class genderPredictor():
     def _nameFeatures(self,name):
         name=name.upper()
         return {
-            'last_letter': name[-1],
-            'last_two' : name[-2:],
             'last_three': name[-3:],
-            'last_is_vowel' : (name[-1] in 'AEIOUY')
+            'first_letter': name[:1]
         }
 
 if __name__ == "__main__":
